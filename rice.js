e3308a2d574a009b91dffb34822f67d9ae52236a
3 <151688670+ICodeUTake@users.noreply.github.com>
2023-11-22 05:39:43 +0000

    Minimal file size + Remove debug

--- rice.js
+++ rice.js
@@ -9,18 +9,9 @@ var FailSafe = "https://example.com/authenticated/false/FailedAuth/";
 let params = new URLSearchParams(window.location.search);
 let authcode = params.get("auth");
 
-console.log(params);
-console.log(authcode);
-console.log(window.location.search);
-
 if(authcode === AuthCode){
-  console.log("valid");
   window.location.href = SendAfterAuth;
 }else{
-  let alert = document.createElement("h1");
-  alert.innertText = "invalid.";
-  alert.appendChild(document.body);
-  console.log("invalid");
   window.location.href = FailSafe;
 }
 
